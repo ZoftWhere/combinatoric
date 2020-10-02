31d2171cdbeac4f3510db6aeddd280a96e83915c
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-02 16:27:34 +0200

    Updated JavaDoc for BigInteger Sequence.

--- main-java/app/zoftwhere/combinatoric/SequenceBigInteger.java
+++ main-java/app/zoftwhere/combinatoric/SequenceBigInteger.java
@@ -2,20 +2,28 @@ package app.zoftwhere.combinatoric;
 
 import java.math.BigInteger;
 
+/**
+ * <p>Sequence BigInteger.
+ * </p>
+ * <p>This is a package-private class that implements functionality.
+ * </p>
+ *
+ * @author Osmund
+ */
 class SequenceBigInteger extends AbstractSequence<BigInteger, BigInteger> {
 
+    /**
+     * Constructor for {@link app.zoftwhere.combinatoric.SequenceBigInteger} (package-private).
+     * @param base base
+     * @param increment increment
+     * @param exponent exponent
+     * @param length length
+     */
     SequenceBigInteger(BigInteger base, BigInteger increment, int exponent, BigInteger length) {
         super(base, increment, exponent, length);
     }
 
-    @Override
-    public BigInteger sum() {
-        return Series.calculate(base(), increment(), exponent(), length());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected Sequence<BigInteger, BigInteger> newInstance(BigInteger base,
         BigInteger increment,
@@ -25,33 +33,31 @@ class SequenceBigInteger extends AbstractSequence<BigInteger, BigInteger> {
         return new SequenceBigInteger(base, increment, exponent, length);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
+    @Override
+    public BigInteger sum() {
+        return Series.calculate(base(), increment(), exponent(), length());
+    }
+
+    /** {@inheritDoc} */
     @Override
     protected BigInteger convertValue(int value) {
         return BigInteger.valueOf(value);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected BigInteger convertValue(long value) {
         return BigInteger.valueOf(value);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected BigInteger convertLength(int length) {
         return BigInteger.valueOf(length);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected BigInteger convertLength(long length) {
         return BigInteger.valueOf(length);
