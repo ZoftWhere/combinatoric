5da30f53e24123e5e43b8838526651e4d700b149
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-02 16:27:25 +0200

    Updated JavaDoc for Primitive Sequence.

--- main-java/app/zoftwhere/combinatoric/SequencePrimitive.java
+++ main-java/app/zoftwhere/combinatoric/SequencePrimitive.java
@@ -1,36 +1,58 @@
 package app.zoftwhere.combinatoric;
 
+/**
+ * <p>Sequence Primitive.
+ * </p>
+ * <p>This is a package-private class that implements functionality.
+ * </p>
+ *
+ * @author Osmund
+ */
 class SequencePrimitive extends AbstractSequence<Long, Long> {
 
+    /**
+     * Constructor for {@link app.zoftwhere.combinatoric.SequencePrimitive} (package-private).
+     *
+     * @param base      base
+     * @param increment increment
+     * @param exponent  exponent
+     * @param length    length
+     */
     SequencePrimitive(Long base, Long increment, int exponent, Long length) {
         super(base, increment, exponent, length);
     }
 
+    /** {@inheritDoc} */
     @Override
     public Long sum() {
         return Series.calculate(base(), increment(), exponent(), length());
     }
 
+    /** {@inheritDoc} */
     @Override
     protected SequencePrimitive newInstance(Long base, Long increment, int exponent, Long length) {
         return new SequencePrimitive(base, increment, exponent, length);
     }
 
+    /** {@inheritDoc} */
     @Override
     protected Long convertValue(int value) {
         return (long) value;
     }
 
+    /** {@inheritDoc} */
     @Override
     protected Long convertValue(long value) {
         return value;
     }
 
+    /** {@inheritDoc} */
     @Override
     protected Long convertLength(int length) {
         return (long) length;
     }
 
+    /** {@inheritDoc} */
     @Override
     protected Long convertLength(long length) {
         return length;
