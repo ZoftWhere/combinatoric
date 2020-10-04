f6e49eefe2bf8508b7b3b7ae8b58de996f41ca3c
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-04 12:39:57 +0200

    Added Calculator with binomial coefficient method.

--- main-java/app/zoftwhere/combinatoric/Calculator.java
+++ main-java/app/zoftwhere/combinatoric/Calculator.java
@@ -48,4 +48,40 @@ public class Calculator {
         return result;
     }
 
+    /**
+     * Utility method for calculating a binomial coefficient.
+     *
+     * @param n number of elements
+     * @param r elements to choose
+     * @return binomial coefficient
+     * @since 3.0.0
+     */
+    public static BigInteger nCr(int n, int r) {
+        if (n < 0) {
+            String message = "combinatoric.calculator.ncr.number.negative";
+            Exception cause = new Exception("n: " + n);
+            throw new IllegalArgumentException(message, cause);
+        }
+
+        if (r < 0 || n < r) {
+            String message = "combinatoric.calculator.ncr.number.out.of.bound";
+            Exception cause = new Exception("r: " + r);
+            throw new IllegalArgumentException(message, cause);
+        }
+
+        if (r == 0 || n == r) {
+            return BigInteger.ONE;
+        }
+
+        final int k = Math.min(r, n - r);
+        BigInteger result = BigInteger.valueOf(n);
+
+        for (var i = 1; i < k; i++) {
+            result = result.multiply(BigInteger.valueOf(n - i));
+            result = result.divide(BigInteger.valueOf(i + 1));
+        }
+
+        return result;
+    }
+
 }
---
+++ test-java/app/zoftwhere/combinatoric/scope/BinomialCoefficientScopeTest.java
@@ -0,0 +1,67 @@
+package app.zoftwhere.combinatoric.scope;
+
+import org.junit.jupiter.api.Test;
+
+import static app.zoftwhere.combinatoric.Calculator.nCr;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class BinomialCoefficientScopeTest {
+
+    @Test
+    void testPascal() {
+        assertEquals(1, nCr(0, 0).intValue());
+
+        assertEquals(1, nCr(1, 0).intValue());
+        assertEquals(1, nCr(1, 1).intValue());
+
+        assertEquals(1, nCr(2, 0).intValue());
+        assertEquals(2, nCr(2, 1).intValue());
+        assertEquals(1, nCr(2, 2).intValue());
+
+        assertEquals(1, nCr(3, 0).intValue());
+        assertEquals(3, nCr(3, 1).intValue());
+        assertEquals(3, nCr(3, 2).intValue());
+        assertEquals(1, nCr(3, 3).intValue());
+    }
+
+    @Test
+    void testNegativeNumber() {
+        try {
+            nCr(-1, 0);
+        }
+        catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertEquals("combinatoric.calculator.ncr.number.negative", e.getMessage());
+            assertNotNull(e.getCause());
+            assertEquals(Exception.class, e.getCause().getClass());
+            assertEquals("n: -1", e.getCause().getMessage());
+        }
+    }
+
+    @Test
+    void testOutOfBoundNumber() {
+        try {
+            nCr(0, -1);
+        }
+        catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertEquals("combinatoric.calculator.ncr.number.out.of.bound", e.getMessage());
+            assertNotNull(e.getCause());
+            assertEquals(Exception.class, e.getCause().getClass());
+            assertEquals("r: -1", e.getCause().getMessage());
+        }
+
+        try {
+            nCr(0, 1);
+        }
+        catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertEquals("combinatoric.calculator.ncr.number.out.of.bound", e.getMessage());
+            assertNotNull(e.getCause());
+            assertEquals(Exception.class, e.getCause().getClass());
+            assertEquals("r: 1", e.getCause().getMessage());
+        }
+    }
+
+}
